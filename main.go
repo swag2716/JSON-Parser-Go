010ede8f175c2e1213896ac1dc145120346bca36
swag2716 <swapnikaagrawal@gmail.com>
2024-09-16 22:22:34 +0530

    added other syntax error conditions, need to modify

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	length := len(str)
 
-	if length == 0 {
+	if length < 2 {
 		fmt.Println("Length = 0")
 		os.Exit(1)
 	}
@@ -29,6 +29,27 @@ func main() {
 	if str[0] != '{' || str[length-1] != '}' {
 		fmt.Println("curly braces error")
 		os.Exit(1)
+	}
+	flag := false
+	for i := 1; i < length-1; i++ {
+		k := i
+		if str[k] != '"' {
+			fmt.Println("syntax error")
+			os.Exit(1)
+		}
+		for str[k] != '"' {
+			k++
+		}
+		if flag && str[k] != ',' {
+			fmt.Println("syntax error")
+			os.Exit(1)
+		}
+		if !flag && str[k] != ':' {
+			fmt.Println("syntax error")
+			os.Exit(1)
+		}
+		flag = !flag
+		i = k
 	}
 
 	fmt.Println(str)
